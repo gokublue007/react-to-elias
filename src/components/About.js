5179225cfb680973f50677d668916630ff1a4172
Elias Vasquez <eliasvasquez7@gmail.com>
2022-07-21 19:20:50 -0600

    Removed extra words

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ const About = () => {
 				id="glass"
 			>
 				<figure className="glass-container-profile " id="profile">
-					<img alt="Marcus" src={MyPhoto} />
+					<img alt="Elias" src={MyPhoto} />
 				</figure>
 				<div className="glass-container-text w-2/3" id="text">
 					<h2 id="glass_title" className="text-2xl pb-2 uppercase">
@@ -18,7 +18,7 @@ const About = () => {
 					<p id="lorem_text" className=" indent-6">
 						Hello, My name is Eli! I'm a Colorado native, based in Denver, Colorado!
 						My journey began in February of 2022 for the Full Stack Developer career after ending my 7 year sales career for Toyota. 
-                        I worked in the industry and was recognized as a top 3 best performance saleman within the company for 5 out of my 7 years of working in the in! 
+                        I worked in the industry and was recognized as a top 3 best performance saleman within the company for 5 out of my 7 years! 
 					</p>
 				</div>
 			</div>
